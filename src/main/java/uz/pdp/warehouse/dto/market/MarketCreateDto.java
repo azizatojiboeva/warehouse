771afaca4de3adb56eb77a751f70d5f0b03b3820
Nodirbeke <nodirbekjuraev02@gmail.com>
2022-03-29 18:20:48 +0500

    SOME

--- src/main/java/uz/pdp/warehouse/dto/market/MarketCreateDto.java
+++ src/main/java/uz/pdp/warehouse/dto/market/MarketCreateDto.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 import uz.pdp.warehouse.dto.base.BaseGenericDto;
 
+import javax.validation.constraints.NotNull;
+
 /**
  * @author Axmadjonov Eliboy, Fri 12:05 AM,3/18/2022
  */
@@ -11,7 +13,7 @@ import uz.pdp.warehouse.dto.base.BaseGenericDto;
 @Getter
 @Setter
 public class MarketCreateDto implements BaseGenericDto {
-
+@NotNull
     private String name;
 
     private Double latitude;
